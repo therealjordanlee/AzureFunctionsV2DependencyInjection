4d0ca21ca5cf0556010fd304bf44dcb887b148b9
Jordan Lee <jordan@jordanlee.net>
2019-05-20 16:47:46 +1000

    First

---
+++ DemoFunction/Repositories/MessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoFunction.Repositories
+{
+    public class MessageRepository
+    { 
+    }
+}
---
+++ DemoFunction/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(DemoFunction.Startup))]
+
+namespace DemoFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Configurations
+            // https://blog.jongallant.com/2018/01/azure-function-config/
+            // https://www.koskila.net/how-to-access-azure-function-apps-settings-from-c/
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.2
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services
+                .Configure
+        }
+    }
+}
